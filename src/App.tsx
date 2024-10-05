b9fb7470ccc60829667bb63ca68425a492b301fc
Michal Gawik <gawlik.zator@o2.pl>
2024-10-05 08:18:34 +0200

    create components and render

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import Gallery from './components/Gallery'
+import SearchForm from './components/SearchForm'
+import ThemeToggle from './components/ThemeToggle'
+
 function App() {
-	return <div className=''></div>
+	return (
+		<>
+			<ThemeToggle />
+			<SearchForm />
+			<Gallery />
+		</>
+	)
 }
 
 export default App
