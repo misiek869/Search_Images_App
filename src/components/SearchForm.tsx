d304a58dbff2732d3b4802436cdc651eb3e5a347
Michal Gawik <gawlik.zator@o2.pl>
2024-10-05 09:52:35 +0200

    style search form

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,7 +1,24 @@
 import React from 'react'
 
 const SearchForm = () => {
-	return <div>SearchForm</div>
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+		console.log(123)
+	}
+
+	return (
+		<section>
+			<form className='search-form' action='' onSubmit={handleSubmit}>
+				<input
+					className='form-input search-input'
+					type='text'
+					name='search'
+					placeholder='search for photo'
+				/>
+				<button type='submit'>Search</button>
+			</form>
+		</section>
+	)
 }
 
 export default SearchForm
