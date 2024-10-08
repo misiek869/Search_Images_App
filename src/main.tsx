f9b134431ad6b71729db1a229485f228c36c0188
Michal Gawik <gawlik.zator@o2.pl>
2024-10-08 08:40:02 +0200

    add ReactQueryDevtools

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { AppContext } from './context.tsx'
 import './index.css'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const queryClient = new QueryClient()
 
@@ -13,6 +14,7 @@ createRoot(document.getElementById('root')!).render(
 		<AppContext>
 			<QueryClientProvider client={queryClient}>
 				<App />
+				<ReactQueryDevtools initialIsOpen={true} />
 			</QueryClientProvider>
 		</AppContext>
 	</StrictMode>
