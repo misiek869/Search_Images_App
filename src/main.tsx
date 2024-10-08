a08f9d29cebef63a702a04c5a69fa8a9870b9685
Michal Gawik <gawlik.zator@o2.pl>
2024-10-08 06:02:55 +0200

    set up react query

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,16 @@ import App from './App.tsx'
 import { AppContext } from './context.tsx'
 import './index.css'
 
-// import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<AppContext>
-			<App />
+			<QueryClientProvider client={queryClient}>
+				<App />
+			</QueryClientProvider>
 		</AppContext>
 	</StrictMode>
 )
